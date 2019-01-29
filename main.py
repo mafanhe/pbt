d84ae87fcc50ced11182b976f2c9c23d890d90f3
17070645 <17070645@cnsuning.com>
2019-01-29 16:35:52 +0800

    update main.py and README.md

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ from model import get_optimizer
 from trainer import Trainer
 from datasets import data
 
-# os.environ['TF_CPP_MIN_LOG_LEVEL'] = '0'  # or any {'0', '1', '2'}
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'  # or any {'0', '1', '2'}
 
 
 def init():
